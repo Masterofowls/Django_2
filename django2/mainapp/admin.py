ebe88be04fa9ea6dd174fd1c58ce361e26a2dfee
masterofowls <mrdaniilsht@gmail.com>
2026-05-11 14:45:47 +0300

    добавил все категории в админ

--- django2/mainapp/admin.py
+++ django2/mainapp/admin.py
@@ -1,10 +1,60 @@
 from django.contrib import admin
-from .models import Product
-# Register your models here.
+from .models import Product, Category, Manufacturer, Supplier, PickUpPoint, Order
+
 
 @admin.register(Product)
-
 class ProductAdmin(admin.ModelAdmin):
-  list_display = ('id', 'article', 'name', 'category', 'manufacturer', 'price', 'supplier', 'stock_quantity')
-  list_filter = ('category', 'manufacturer', 'price', 'stock_quantity', 'supplier')
-  search_fields = ('name', 'article', 'description')
+    list_display = (
+        "id",
+        "article",
+        "name",
+        "category",
+        "manufacturer",
+        "price",
+        "supplier",
+        "stock_quantity",
+    )
+    list_filter = ("category", "manufacturer", "price", "stock_quantity", "supplier")
+    search_fields = ("name", "article", "description")
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+    list_filter = ("name",)
+    search_fields = ("name",)
+
+
+@admin.register(Manufacturer)
+class ManufacturerAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+    list_filter = ("name",)
+    search_fields = ("name",)
+
+
+@admin.register(Supplier)
+class SupplierAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+    list_filter = ("name",)
+    search_fields = ("name",)
+
+
+@admin.register(PickUpPoint)
+class PickUpPointAdmin(admin.ModelAdmin):
+    list_display = ("id", "address")  # ← поле address, а не name
+    list_filter = ("address",)  # ← опционально
+    search_fields = ("address",)
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "number",
+        "order_date",
+        "delivery_date",
+        "pickup_point",
+        "status",
+    )
+    list_filter = ("status", "order_date", "delivery_date")
+    search_fields = ("number",)
