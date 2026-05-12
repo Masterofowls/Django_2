39018de0755158b3e4430d2bf245393168cf4fd7
masterofowls <mrdaniilsht@gmail.com>
2026-05-12 12:12:14 +0300

    todo

--- django2/mainapp/urls.py
+++ django2/mainapp/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("products/", views.ProductListView.as_view(), name="product_list"),
 ]
--- django2/mainapp/views.py
+++ django2/mainapp/views.py
@@ -1,5 +1,12 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+from django.views.generic import ListView
 
+from .models import Product
 
 def home(request):
-    return HttpResponse("Hello, this is the homepage.")
+    return render(request, 'mainapp/home.html')
+
+class ProductListView(ListView):
+    model = Product
+    template_name = 'mainapp/product_list.html'
+    context_object_name = 'products'
