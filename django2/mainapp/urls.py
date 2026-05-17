c14bf341acc644ef372046478737db1efdbf8639
masterofowls <mrdaniilsht@gmail.com>
2026-05-17 17:10:01 +0300

    вход

--- django2/mainapp/urls.py
+++ django2/mainapp/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
     path("products/", views.ProductListView.as_view(), name="product_list"),
+    path("products/partial/", views.product_list_partial, name="product_list_partial"),
+    path("products/create/", views.ProductCreateView.as_view(), name="product_create"),
 ]
--- django2/mainapp/views.py
+++ django2/mainapp/views.py
@@ -1,12 +1,58 @@
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, ListView
 
 from .models import Product
 
+
+def is_admin(user):
+    """Возвращает True, если пользователь аутентифицирован и имеет роль admin."""
+    return user.is_authenticated and user.role == "admin"
+
+
 def home(request):
-    return render(request, 'mainapp/home.html')
+    return render(request, "mainapp/home.html")
+
 
 class ProductListView(ListView):
     model = Product
-    template_name = 'mainapp/product_list.html'
-    context_object_name = 'products'
+    template_name = "mainapp/product_list.html"
+    context_object_name = "products"
+
+
+def product_list_partial(request):
+    # возвращает html фрагмент
+    products = Product.objects.select_related(
+        "category", "manufacturer", "supplier"
+    ).order_by("name")
+
+    return render(
+        request,
+        "mainapp/_product_table.html",
+        {"products": products},
+    )
+
+
+class ProductCreateView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
+    """Форма создания нового товара. Доступна только пользователям с ролью admin."""
+
+    model = Product
+    template_name = "mainapp/product_form.html"
+    fields = [
+        "article",
+        "name",
+        "description",
+        "price",
+        "unit",
+        "stock_quantity",
+        "discount",
+        "photo",
+        "category",
+        "manufacturer",
+        "supplier",
+    ]
+    success_url = reverse_lazy("product_list")
+
+    def test_func(self):
+        return is_admin(self.request.user)
